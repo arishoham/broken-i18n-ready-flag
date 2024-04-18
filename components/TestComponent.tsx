865ef83fc89676ebe48d91a6ee0da82dd0133bb1
Ari Shoham <ashoham@shutterstock.com>
2024-04-18 13:21:04 -0500

    cleanup

--- components/TestComponent.tsx
+++ components/TestComponent.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import { useTranslation } from 'next-i18next'
 import type { FC } from 'react'
 
@@ -7,9 +6,5 @@ type Props = {}
 export const TestComponent: FC<Props> = () => {
   const { t, ready } = useTranslation('second-page');
 
-  return (
-    <>
-      <div>{ready ? t('title') : 'Loading...'}</div>
-    </>
-  )
+  return <div>{ready ? t('title') : 'Loading...'}</div>
 }
