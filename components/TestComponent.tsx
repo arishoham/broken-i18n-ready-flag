f861eb21c650b4bc5900b8e79787d146d76dd12b
Ari Shoham <ashoham@shutterstock.com>
2024-04-18 13:10:10 -0500

    Broken ready flag

---
+++ components/TestComponent.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import { useTranslation } from 'next-i18next'
+import type { FC } from 'react'
+
+type Props = {
+  heading: string
+  title: string
+}
+
+export const TestComponent: FC<Props> = () => {
+  const { t, ready } = useTranslation('second-page');
+
+  return (
+    <>
+      <div>{ready ? t('title') : 'Loading...'}</div>
+    </>
+  )
+}
